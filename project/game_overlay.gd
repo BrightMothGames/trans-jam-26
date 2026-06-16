85a50f3f36c804d7a363e52f2536d8e0d628c6a6
Rook <rook@trashbird.gay>
2026-06-16 16:42:25 -0500

    testing shaderzz

---
+++ project/game_overlay.gd
@@ -0,0 +1,26 @@
+extends SubViewportContainer
+
+@onready var shader = material
+
+var shadow_offsest: Vector2 = Vector2.ZERO
+
+
+func _on_lcd_gap_value_changed(value: float) -> void:
+	shader.set_shader_parameter("scanline_width", value)
+
+func _on_lcd_str_value_changed(value: float) -> void:
+	shader.set_shader_parameter("scanline_strength", value)
+	
+func _on_shad_x_value_changed(value: float) -> void:
+	shadow_offsest.x = value
+	shader.set_shader_parameter("shadow_displacement", shadow_offsest)
+
+func _on_shad_y_value_changed(value: float) -> void:
+	shadow_offsest.y = value
+	shader.set_shader_parameter("shadow_displacement", shadow_offsest)
+
+func _on_shad_str_value_changed(value: float) -> void:
+	shader.set_shader_parameter("shadow_strength", value)
+
+func _on_bright_value_changed(value: float) -> void:
+	shader.set_shader_parameter("brightness", value)
